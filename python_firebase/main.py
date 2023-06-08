f0364b671fdf983492fddcbb3b87c423cdcda6d6
GiannettaGerardo <ggp00519@protonmail.com>
2023-06-09 01:32:38 +0200

    Now there is only one main.py

--- python_firebase/main.py
+++ python_firebase/main.py
@@ -1,50 +1,44 @@
+from views.viewinterface import ViewInterface
+from views.authview import AuthView
 from my_firebase_client import FirebaseClient
 
-def print_auth_menu():
-  print(" ||----------------------------------------------------||")
-  print(" || Welcome to ...                                     ||")
-  print(" ||----------------------------------------------------||")
-  print(" ||                        Menù                        ||")
-  print(" || Enter a number to choose an action.                ||")
-  print(" || [1] Exit                                           ||")
-  print(" || [2] Sign Up                                        ||")
-  print(" || [3] Sign In                                        ||")
-  print(" ||----------------------------------------------------||")
+class ViewStack:
+  def __init__(self, firstView: ViewInterface=None):
+    self._viewStack = []
+    if firstView != None:
+      if not isinstance(firstView, ViewInterface):
+        raise ValueError('ViewInterface allowed only')
+      self._viewStack.append(firstView)
+  
+  def push(self, value: ViewInterface):
+    if not isinstance(value, ViewInterface):
+      raise ValueError('ViewInterface allowed only')
+    self._viewStack.append(value)
+  
+  def pop(self) -> ViewInterface:
+    return self._viewStack.pop()
+  
+  def get(self) -> ViewInterface:
+    return self._viewStack[-1]
+  
+  def isEmpty(self) -> bool:
+    return len(self._viewStack) == 0
 
-def print_action_menu(username: str):
-  print("    Welcome " + username)
-  print(" ||----------------------------------------------------||")
-  print(" ||                        Menù                        ||")
-  print(" || Enter a number to choose an action.                ||")
-  print(" || [1] Exit                                           ||")
-  print(" || [2] Logout                                         ||")
-  print(" ||----------------------------------------------------||")
-
-def print_menu(client: FirebaseClient):
-  if client.isLoggedIn():
-    print_action_menu("User")
-  else:
-    print_auth_menu()
-
-def parse_input(client: FirebaseClient, value: int):
-  if value == 1:
-    print("Exit in progress...")
-    exit()
-  if client.isLoggedIn():
-    pass
 
 def main():
-  client = FirebaseClient()
-  while True:
-    try:
-      print_menu(client)
-      print(">> ", end="")
-      value = int(input())
-      parse_input(client, value)
-    except ValueError:
-      print("Invalid number. Retry...")
-    print("")
-  #client.signUp(email="giannettagerry90@gmail.com", password="f1rebaseTest_")
+  try:
+    client = FirebaseClient()
+    viewStack = ViewStack(AuthView(client))
+    while not viewStack.isEmpty():
+      currentView = viewStack.get()
+      newView = currentView.render()
+      if newView != None:
+        viewStack.push(newView)
+      else:
+        viewStack.pop()
+  except KeyboardInterrupt:
+    pass
+  print("Exit in progress...")
 
 if __name__ == "__main__":
   main()
